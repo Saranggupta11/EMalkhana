1899f5220b69f3355636bcc788d87f94dd2295e0
Rishav Singh Solanki <82879807+solankirishav21@users.noreply.github.com>
2023-12-09 23:01:48 +0530

    dashboard-frontend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -19,15 +19,15 @@ const Login = () => {
 
   return (
     <>
-      <section class="text-gray-600 body-font relative">
-        <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap justify-center">
-          <div class=" lg:w-1/4">
-            <h2 class="text-gray-900 text-center text-2xl mb-1 font-medium title-font">
+      <section className="text-gray-600 body-font relative">
+        <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap justify-center">
+          <div className=" lg:w-1/4">
+            <h2 className="text-gray-900 text-center text-2xl mb-1 font-medium title-font">
               IO Login
             </h2>
 
-            <div class="relative mb-4">
-              <label for="userId" class="leading-7 text-sm text-gray-600">
+            <div className="relative mb-4">
+              <label for="userId" className="leading-7 text-sm text-gray-600">
                 userId
               </label>
               <input
@@ -35,11 +35,11 @@ const Login = () => {
                 id="userId"
                 name="userId"
                 onChange={(e) => setuserId(e.target.value)}
-                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
-            <div class="relative mb-4">
-              <label for="password" class="leading-7 text-sm text-gray-600">
+            <div className="relative mb-4">
+              <label for="password" className="leading-7 text-sm text-gray-600">
                 Password
               </label>
               <input
@@ -47,14 +47,14 @@ const Login = () => {
                 id="password"
                 name="password"
                 onChange={(e) => setPassword(e.target.value)}
-                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
 
             <div className="flex justify-center">
             <button
               onClick={handleLogin}
-              class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+              className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
             >
               Login
             </button>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Login from '../components/Login'
+import Dashboard from '@/components/Dashboard'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <Login/>
+    <>
+      {/* <Login/> */}
+      <Dashboard/>
+    </>
   )
 }
