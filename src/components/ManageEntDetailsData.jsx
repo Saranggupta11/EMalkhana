d2b9cacd202bb03a4dc5f06de03c45a541d03621
saranggupta11 <sarang.gupta0811@gmail.com>
2023-12-13 02:16:11 +0530

    image fix

--- src/components/ManageEntDetailsData.jsx
+++ src/components/ManageEntDetailsData.jsx
@@ -242,9 +242,10 @@ const ManageEntDetailsData = () => {
                           <Modal
                             open={openDetails}
                             onClose={handleCloseDetails}
-                            style={{ overflow: "auto" }}
+                            style={{ overflow: "scroll" }}
                             aria-labelledby="modal-modal-title"
                             aria-describedby="modal-modal-description"
+                            
                           >
                             <Box sx={style}>
                               <Typography
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,22 +6,21 @@ export default function Navbar() {
   return (
     <div>
       <div className="flex justify-center mt-10">
-        {/* <Logo/> */}
-        {/* <Image
-             src="/public/images/logo.png"
-             width={500}
-             height={500}
+        <Image
+             src="/images/logo.png"
+             width={150}
+             height={150}
              alt='Logo'
-            /> */}
+            />
         <div className="text-5xl font-black text-cente text-blue-600">
           Jharkhand Police E-Malkhana
         </div>
-        {/* <Image
-             src="/public/images/logo.png"
-             width={500}
-             height={500}
+        <Image
+             src="/images/logo.png"
+             width={150}
+             height={150}
              alt='Logo'
-            /> */}
+            />
       </div>
 
       <div className="my-9 flex bg-blue-500 justify-center py-3 text-2xl">
